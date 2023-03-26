170c0fda740902e9ceaac008a5f4f5fff6cae718
Jim Farrell <farrelljames558@gmail.com>
2023-03-26 01:27:18 +0100

    Initial commit.

---
+++ _next/static/9Qpf15HihhtLMVqeH9dMH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/573-a192fe3770f43455.js","static/chunks/pages/index-00079d703fba4d65.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
